2441ee6ee2d6f79cdcaaa9646c92c9b8938a1511
novuh <56933503+n0vuh@users.noreply.github.com>
2021-01-27 21:02:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -28,15 +28,10 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).sendFile(path.join(__dirname, '/public/500.html'))
+})
 
 app.listen(3000);
 
